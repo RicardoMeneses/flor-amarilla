b5bc6def15867dc5b2732ea8fa04e6176f67cd56
RicardoM18 <ricardomeneses0503@gmail.com>
2023-09-21 02:51:14 -0600

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Flor from './components/Flor';
+import './app.css';
 
 function App() {
   return (
@@ -14,7 +14,34 @@ function App() {
         <h1 style={{ color: 'white' }}>Te quiero Mucho</h1>
       </div>
       <div>
-        <Flor />
+        <div className='flower_wrapper'>
+          <div className='flower_stem'></div>
+          <div className='flower_center'></div>
+          <div className='flower_petal flower_petal-1'></div>
+          <div className='flower_petal flower_petal-2'></div>
+          <div className='flower_petal flower_petal-3'></div>
+          <div className='flower_petal flower_petal-4'></div>
+          <div className='flower_petal flower_petal-5'></div>
+          <div className='flower_petal flower_petal-6'></div>
+          <div className='flower_petal flower_petal-7'></div>
+          <div className='flower_petal flower_petal-8'></div>
+          <div className='flower_petal flower_petal-9'></div>
+          <div className='flower_petal flower_petal-10'></div>
+          <div className='flower_petal flower_petal-11'></div>
+          <div className='flower_petal flower_petal-12'></div>
+
+          <div className='flower_leaf flower_leaf-1'></div>
+          <div className='flower_leaf flower_leaf-2'></div>
+
+          <div className='flower_light flower_light-1'></div>
+          <div className='flower_light flower_light-2'></div>
+          <div className='flower_light flower_light-3'></div>
+          <div className='flower_light flower_light-4'></div>
+          <div className='flower_light flower_light-5'></div>
+          <div className='flower_light flower_light-6'></div>
+          <div className='flower_light flower_light-7'></div>
+          <div className='flower_light flower_light-8'></div>
+        </div>
       </div>
 
       <div className='light light-1'></div>
